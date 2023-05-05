80a42ecad60afd253fbf5c605178273cf3bf8dfc
felipedanielfreitasdesouza <132684205+felipedanielfreitasdesouza@users.noreply.github.com>
2023-05-05 12:34:43 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,25 +1,41 @@
-function mapa(params) {   
-var jogador = "&"
-var chave = "@"
-var porta = "D"
 var mapa = [["*","*","*","*","*",],
-["*","0","0","0","*",],
-["*","0","0","0","*",],
-["*","0","0","0","*",],                    
-["*","*","*","*","*"]]
+            ["*","0","0","0","*",],
+            ["*","0","0","0","*",],
+            ["*","0","0","0","*",],                    
+            ["*","*","*","*","*"]]
+
+
 document.getElementById("mapa").innerHTML = mapa
 
 
-}
+
+var jogador = "&"
+//matriz [0][0]
+
+//var chave = "@"
+
+
+//var porta = "D"
+
+
 window.addEventListener("keydown",function keydown(event){
 var tecla = event.keyCode
-this.alert(tecla)
 
 
-if (tecla ==="W") {
-   
-  
+if (tecla.key ==="W") {
+   //matriz[0] [-1] //muda a posição do jogador
+  //dps ele faz uma atualização no mapa
 }
-
-
+if (tecla.key ==="S") {
+   // matriz[0] [+1] //muda a posição do jogador
+   //dps ele faz uma atualização no mapa
+ }
+ if (tecla.key ==="D") {
+   // matriz[+1][0] //muda a posição do jogador
+   //dps ele faz uma atualização no mapa
+ }
+ if (tecla.key ==="A") {
+   // matriz[-1][0] //muda a posição do jogador
+   //dps ele faz uma atualização no mapa
+ }
 })
