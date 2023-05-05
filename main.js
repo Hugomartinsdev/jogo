40dbbacf3aa2168c258e8529752399412b9ffb62
felipedanielfreitasdesouza <132684205+felipedanielfreitasdesouza@users.noreply.github.com>
2023-05-05 11:41:28 -0300

    Add files via upload
    TESTE

---
+++ main.js
@@ -0,0 +1,22 @@
+var mapa = [[
+"*","*","*","*","*",
+"*"," "," "," ","*",
+"*"," "," "," ","*",
+"*"," "," "," ","*",                     
+"*","*","*","*","*"]]
+document.getElementById("teste").innerHTML = mapa
+
+window.addEventListener("keydown",function keydown(event){
+var tecla = event.keyCode
+this.alert(tecla)
+
+
+if (tecla ==="W") {
+   
+  
+}
+
+
+})
+
+
