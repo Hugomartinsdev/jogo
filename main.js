614227dd0fb5f89cbf448361f937570e87de5f13
Ana Beatriz Nunes <124877013+Ana-Beatriz-N@users.noreply.github.com>
2023-05-07 21:22:24 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,41 +1,113 @@
-var mapa = [["*","*","*","*","*",],
-            ["*","0","0","0","*",],
-            ["*","0","0","0","*",],
-            ["*","0","0","0","*",],                    
-            ["*","*","*","*","*"]]
+function teste(params) {
+   var mapa1=['*','*','*','*','*','*','*','*','*','*','*','*','*','*','*',`<br>`];
+   var mapa2=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa3=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa4=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa5=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa6=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa7=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa8=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa9=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa10=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa11=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa12=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa13=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa14=['*','0','0','0','0','0','0','0','0','0','0','0','0','0','*',`<br>`];
+   var mapa15=['*','*','*','*','*','*','*','*','*','*','*','*','*','*','*'];
+var a;
+var b;
+var c;
+var d;
+var e;
+var k;
+var p;
+var q;
+var n;
+var l;
+var w;
+var o;
+var x;
+var y;
+var z;
+y=""
+x=""
+z=""
+a=""
+b=""
+c=""
+d=""
+e=""
+k=""
+p=""
+q=""
+l=""
+w=""
+o=""
+n=""
 
 
-document.getElementById("mapa").innerHTML = mapa
-
-
-
-var jogador = "&"
-//matriz [0][0]
-
-//var chave = "@"
-
-
-//var porta = "D"
-
-
-window.addEventListener("keydown",function keydown(event){
-var tecla = event.keyCode
-
-
-if (tecla.key ==="W") {
-   //matriz[0] [-1] //muda a posição do jogador
-  //dps ele faz uma atualização no mapa
+for (let i = 0; i < mapa1.length; i++) {
+    y += mapa1[i];
+    
 }
-if (tecla.key ==="S") {
-   // matriz[0] [+1] //muda a posição do jogador
-   //dps ele faz uma atualização no mapa
- }
- if (tecla.key ==="D") {
-   // matriz[+1][0] //muda a posição do jogador
-   //dps ele faz uma atualização no mapa
- }
- if (tecla.key ==="A") {
-   // matriz[-1][0] //muda a posição do jogador
-   //dps ele faz uma atualização no mapa
- }
-})
+for (let i = 0; i < mapa2.length; i++) {
+    x += mapa2[i];
+    
+}
+for (let i = 0; i < mapa3.length; i++) {
+    z += mapa3[i];
+    
+}
+for (let i = 0; i < mapa4.length; i++) {
+    a += mapa4[i];
+    
+}
+for (let i = 0; i < mapa5.length; i++) {
+    b += mapa5[i];
+    
+}
+for (let i = 0; i < mapa6.length; i++) {
+    c += mapa6[i];
+    
+}
+for (let i = 0; i < mapa7.length; i++) {
+    d += mapa7[i];
+    
+}
+for (let i = 0; i < mapa8.length; i++) {
+    e += mapa8[i];
+    
+}
+for (let i = 0; i < mapa9.length; i++) {
+    k += mapa9[i];
+    
+}
+for (let i = 0; i < mapa10.length; i++) {
+    p += mapa10[i];
+    
+}
+for (let i = 0; i < mapa11.length; i++) {
+    q += mapa11[i];
+    
+}
+for (let i = 0; i < mapa12.length; i++) {
+    n += mapa12[i];
+    
+}
+for (let i = 0; i < mapa13.length; i++) {
+    l += mapa13[i];
+    
+}
+for (let i = 0; i < mapa14.length; i++) {
+    w += mapa14[i];
+    
+}
+for (let i = 0; i < mapa15.length; i++) {
+    o += mapa15[i];
+    
+}
+
+
+
+          document.getElementById("m").innerHTML=y+x+z+a+b+c+d+e+k+p+q+n+l+w+o
+}
